a57de9ee286c488ff406a97ac62ebba67da108db
Kate Skorija <kateskorija@gmail.com>
2020-08-21 08:46:28 -0700

    create parks model and context model

---
+++ ParksAPI/Models/Park.cs
@@ -0,0 +1,11 @@
+namespace ParksAPI.Models
+{
+    public class Park
+    {
+        public int ParkId { get; set; }
+        public string Name { get; set; }
+        public string State { get; set; }
+        public string Type { get; set; }
+        public bool Camping { get; set; }
+    }
+}
