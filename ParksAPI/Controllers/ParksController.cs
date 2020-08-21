9864b6692bda19eed1dec9445d858951fa7103f6
Kate Skorija <kateskorija@gmail.com>
2020-08-21 09:22:44 -0700

    create functioning put and delete methods

--- ParksAPI/Controllers/ParksController.cs
+++ ParksAPI/Controllers/ParksController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using ParksAPI.Models;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,6 +24,25 @@ namespace ParksAPI.Controllers
     public void Post([FromBody] Park park)
     {
       _db.Parks.Add(park);
+      _db.SaveChanges();
+    }
+    [HttpGet("{id}")]
+    public ActionResult<Park> Get(int id)
+    {
+      return _db.Parks.FirstOrDefault(entry => entry.ParkId == id);
+    }
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] Park park)
+    {
+      park.ParkId = id;
+      _db.Entry(park).State = EntityState.Modified;
+      _db.SaveChanges();
+    }
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+      var parkToDelete = _db.Parks.FirstOrDefault(entry => entry.ParkId == id);
+      _db.Parks.Remove(parkToDelete);
       _db.SaveChanges();
     }
   }
